26e00e73bce226f5227b85a3581dfdaa4a4694ca
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-01-18 22:42:03 +0300

    Added h_queue service.

--- src/queue.cpp
+++ src/queue.cpp
@@ -1,42 +1,44 @@
 #include <ros/ros.h>
-#include <ozurover_messages/Enqueue.h>
-#include <ozurover_messages/Dequeue.h>
+#include <ozurover_messages/AddMarker.h>
+#include <ozurover_messages/GetMarker.h>
 #include <ozurover_messages/Abort.h>
+#include <vector>
 #include <deque>
 
-struct QueueElement {
+struct MarkerElement {
     /* Fill this place up with the relevant data*/
 };
 
 class QueueNode {
 private:
-    std::deque<QueueElement> queue;
-    std::deque<QueueElement> history;
+    std::deque<MarkerElement> queue;
+    std::vector<MarkerElement> history;
     ros::NodeHandle nh;
     ros::ServiceServer abort_service;
     ros::ServiceServer queue_service;
     ros::ServiceServer dequeue_service;
-    ros::ServiceServer h_queue_service;
-    ros::ServiceServer h_dequeue_service;
+    ros::ServiceServer h_push_service;
+    ros::ServiceServer h_pop_service;
     
     bool abort(ozurover_messages::Abort::Request &req, ozurover_messages::Abort::Response &res) {
         return true;
     }
 
-    bool enqueue(ozurover_messages::Enqueue::Request &req, ozurover_messages::Enqueue::Response &res) {
+    bool enqueue(ozurover_messages::AddMarker::Request &req, ozurover_messages::AddMarker::Response &res) {
         return true;
     }
-    bool dequeue(ozurover_messages::Dequeue::Request &req, ozurover_messages::Dequeue::Response &res) {
-        return true;
-    }
-
-    bool h_queue_service(ozurover_messages::Dequeue::Request &req, ozurover_messages::Dequeue::Response &res) {
+    bool dequeue(ozurover_messages::GetMarker::Request &req, ozurover_messages::GetMarker::Response &res) {
         return true;
     }
 
-    bool h_dequeue_service(ozurover_messages::Dequeue::Request &req, ozurover_messages::Dequeue::Response &res) {
+    bool h_push(ozurover_messages::AddMarker::Request &req, ozurover_messages::AddMarker::Response &res) {
         return true;
     }
+
+    bool h_pop(ozurover_messages::GetMarker::Request &req, ozurover_messages::GetMarker::Response &res) {
+        return true;
+    }
+
 
 public:
     QueueNode() {
