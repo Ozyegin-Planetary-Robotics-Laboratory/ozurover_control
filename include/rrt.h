f094444c11972b191558aef4f4327eea3f97bbb6
merttkrkmazz <mert.korkmaz.34587@ozu.edu.tr>
2024-03-19 21:23:52 +0300

    fast RRT update

--- include/rrt.h
+++ include/rrt.h
@@ -21,7 +21,6 @@ public:
   geometry_msgs::PoseStamped randomPose();
   int nearestNeighborIndex(geometry_msgs::PoseStamped target);
   geometry_msgs::PoseStamped newPose(geometry_msgs::PoseStamped nearest, geometry_msgs::PoseStamped target);
-  // void RRT::generateRRT(int iterations);
   double distance(geometry_msgs::PoseStamped p1, geometry_msgs::PoseStamped p2);
   std::vector<geometry_msgs::PoseStamped> nodes;
   std::vector<int> parentIndices;
@@ -64,28 +63,27 @@ int RRT::nearestNeighborIndex(geometry_msgs::PoseStamped target) {
       minIndex = i;
     }
   }
-
   return minIndex;
 }
 
 geometry_msgs::PoseStamped RRT::newPose(geometry_msgs::PoseStamped nearest, geometry_msgs::PoseStamped target) {
   double angle = atan2(target.pose.position.y - nearest.pose.position.y, target.pose.position.x - nearest.pose.position.x);
+
+  // Interpolate the direction to bias towards the current direction
+  double parentAngle = atan2(nearest.pose.position.y - nodes[parentIndices.back()].pose.position.y, nearest.pose.position.x - nodes[parentIndices.back()].pose.position.x);
+  angle = 0.8 * angle + 0.2 * parentAngle;
   double x = nearest.pose.position.x + stepSize * cos(angle);
   double y = nearest.pose.position.y + stepSize * sin(angle);
 
   geometry_msgs::PoseStamped newPose;
-  newPose.header.stamp = ros::Time::now();
-  newPose.header.frame_id = "map";
   newPose.pose.position.x = x;
   newPose.pose.position.y = y;
   return newPose;
 }
 
-
 double RRT::distance(geometry_msgs::PoseStamped p1, geometry_msgs::PoseStamped p2) {
   return sqrt(pow(p1.pose.position.x - p2.pose.position.x, 2) + pow(p1.pose.position.y - p2.pose.position.y, 2));
 }
-
 
 nav_msgs::Path RRT::getPath() {
   nav_msgs::Path path;
@@ -93,59 +91,28 @@ nav_msgs::Path RRT::getPath() {
   path.header.frame_id = "map";
 
   int currentIndex = nodes.size() - 1;
-
-
-  //Attention to the following line!
   path.poses.push_back(goal);
-  
   // Iterate from the goal back to the start
   while (currentIndex != 0) {
     geometry_msgs::PoseStamped current_pose = nodes[currentIndex];
-
-
     // Calculates the direction from the current pose to the next pose
     geometry_msgs::PoseStamped next_pose = nodes[parentIndices[currentIndex]];
     double dx = current_pose.pose.position.x - next_pose.pose.position.x;
     double dy = current_pose.pose.position.y - next_pose.pose.position.y;
     double yaw = atan2(dy, dx);
-
     // Convert yaw to a quaternion
     tf2::Quaternion quaternion;
     quaternion.setRPY(0, 0, yaw);
-
     // Set the orientation of the current pose
     next_pose.pose.orientation.x = quaternion.getX();
     next_pose.pose.orientation.y = quaternion.getY();
     next_pose.pose.orientation.z = quaternion.getZ();
     next_pose.pose.orientation.w = quaternion.getW();
-    
     // Add the current pose to the path
     path.poses.push_back(next_pose);
-
     currentIndex = parentIndices[currentIndex];
   }
-
   // Reverse the path to make it start from the beginning
   std::reverse(path.poses.begin(), path.poses.end());
-
   return path;
 }
-
-/* void RRT::generateRRT(int iterations) {
-  for (int i = 0; i < iterations; ++i) {
-    geometry_msgs::PoseStamped randomPoseResult = randomPose();
-    int nearestIndex = nearestNeighborIndex(randomPoseResult);
-    geometry_msgs::PoseStamped nearestNode = nodes[nearestIndex];
-    geometry_msgs::PoseStamped newNode = newPose(nearestNode, randomPoseResult);
-    if (isCollisionFree(nearestNode, newNode)) {
-      nodes.push_back(newNode);
-      parentIndices.push_back(nearestIndex);
-      // Check if the new node is close to the goal
-      if (distance(newNode, goal) < stepSize) {
-        // If close to the goal, break the loop
-        break;
-      }
-    }
-  }
-} */
-
